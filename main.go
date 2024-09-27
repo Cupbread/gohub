670b0a8ae961b44a35fe14dfbb978d0527f982d6
wushucan <wushucan@wps.cn>
2024-09-27 10:23:34 +0800

    初始化路由

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
+	"gohub/bootstrap"
 )
 
 func main() {
 	r := gin.New()
 
-	r.Use(gin.Logger(), gin.Recovery())
+	bootstrap.SetupRoute(r)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"hello": "world",
-		})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
-
-	r.Run(":8080")
+	err := r.Run(":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
