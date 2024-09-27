a9faf3da2d146ef5d7955eab72a1f75da6656510
wushucan <wushucan@wps.cn>
2024-09-27 14:31:21 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	r := gin.New()
 
 	bootstrap.SetupRoute(r)
 
-	err := r.Run(":3000")
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
 }
